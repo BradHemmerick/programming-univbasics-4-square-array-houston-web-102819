3bfb403944e0033f2fc76f00325f63e6de4c9a64
brad hemmerick <brad.hemmerick13@gmail.com>
2019-10-22 22:44:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
 counter = 0
+array2 = []
  while counter < array.length do
-  array[counter] ** 2
+  new_num = array[counter] ** 2
    counter += 1
+   array2 << new_num
   end
 end
